4cb0f834ba9d031111f38aae10a1fea3f9e11278
Anaycoder <84620347+Anaycoder@users.noreply.github.com>
2021-06-03 13:02:17 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+ function one(){
+    document.location = "#second";
+}
+
+function two(){
+    document.location = "#favplayer";
+}
+
+function three(){
+    document.location = "#facts";
+}
+
+function four(){
+    document.location = "#inspiration";
+}
+
+function five(){
+    document.location = "#contact";
+}
